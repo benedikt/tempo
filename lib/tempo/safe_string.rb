e213bda7b596300af666bbf6b79132df05c2f93e
Benedikt Deicke <benedikt@synatic.net>
2013-12-18 16:24:36 +0100

    Makes sure Tempo::SafeStrings stay safe

--- lib/tempo/safe_string.rb
+++ lib/tempo/safe_string.rb
@@ -1,4 +1,7 @@
 module Tempo
   class SafeString < String
+    def to_s
+      self
+    end
   end
 end
--- spec/tempo/stafe_string_spec.rb
+++ spec/tempo/stafe_string_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe Tempo::SafeString do
   it { should be_kind_of(String) }
+
+  describe '#to_s' do
+    it 'should return a safe string' do
+      expect(subject.to_s).to be_kind_of(Tempo::SafeString)
+    end
+  end
 end
