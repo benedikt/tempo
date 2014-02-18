9348c952085bf4264c53644415fbba1874d0ed75
Benedikt Deicke <benedikt@synatic.net>
2014-02-18 13:30:18 +0100

    Extracts Tempo::Runtime#render_with_environment into its own method

--- lib/tempo/runtime.rb
+++ lib/tempo/runtime.rb
@@ -26,9 +26,12 @@ module Tempo
         :options => options
       })
 
+      render_with_environment(template, environment)
+    end
+
+    def render_with_environment(template, environment)
       visitor = Tempo::Visitors::Interpreter.new(self, environment)
       visitor.visit(template)
     end
-
   end
 end
